af2a5f0f0188798cea01eba062b3d34b9d1f0bb8
ASPCartman <aspcartman@gmail.com>
2017-04-05 23:54:22 +0300

    TeamCity addr typo fix

--- server/services/stats/side/tc_client.go
+++ server/services/stats/side/tc_client.go
@@ -24,7 +24,7 @@ func NewTcClient(config Config) TcClient {
 	}
 
 	return &tcClient{
-		apiClient: teamcity.NewClient("https://localhost:8111", teamcity.GuestAuth()),
+		apiClient: teamcity.NewClient(config.TeamCityAddr, teamcity.GuestAuth()),
 	}
 }
 
